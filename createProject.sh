926870ec8391af20730fcfa2b04a37307da3009c
Ralf Waspe <raw@mmmi.sdu.dk>
2021-11-15 13:18:29 +0100

    updated clang format

--- createProject.sh
+++ createProject.sh
@@ -3,12 +3,6 @@
 PROJECT_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"  
 PROJECT=$(pwd | awk -F / '{print $NF}')
 
-echo "create gitignore in $PROJECT"
-
-echo "/bin" > .gitignore
-echo "**/*.user" >> .gitignore
-echo "**/build-*" >> .gitignore
-
 mkdir src
 cd src
 
@@ -27,13 +21,21 @@ echo "DESTDIR = \$\$PWD/../bin" >> $PRO_FILE
 
 echo "" >> $PRO_FILE
 
-cp $PROJECT_SCRIPT_DIR/_clang-format .clang-format
-
 cd ..
+
+echo "create gitignore in $PROJECT"
+
+echo "/bin" > .gitignore
+echo "**/*.user" >> .gitignore
+echo "**/build-*" >> .gitignore
+
+cp $PROJECT_SCRIPT_DIR/_clang-format _clang-format
+
 if [ -d .git ]
 then
    git add .gitignore
    git add src/*
-   git add src/.clang-format
+   git add src/_clang-format
    git commit -m "project infrastructure"
 fi
+
