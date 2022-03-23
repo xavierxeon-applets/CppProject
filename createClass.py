96b2086675d77b8940ea1eb5e56ca3970f54d3e6
Ralf Waspe <rwaspe@icloud.com>
2022-03-23 17:18:34 +0100

    inlne include guards

--- createClass.py
+++ createClass.py
@@ -1,143 +1,153 @@
 #!/usr/bin/env python3
 
-import os, sys, argparse
+import os
+import sys
+import argparse
 
 
 def fullName(className, nameSpaces):
 
-   if nameSpaces:
-      return ''.join(nameSpaces) + className
-   else:
-      return className
+    if nameSpaces:
+        return ''.join(nameSpaces) + className
+    else:
+        return className
+
 
 def fullNameSpace(nameSpaces):
 
-   if nameSpaces:
-      return '::'.join(nameSpaces) + '::'
-   else:
-      return ''
+    if nameSpaces:
+        return '::'.join(nameSpaces) + '::'
+    else:
+        return ''
+
 
 def abortIfExists(fileName):
 
-   if not os.path.exists(fileName):
-      return
+    if not os.path.exists(fileName):
+        return
 
-   print(fileName, 'already exists')
-   sys.exit(1)
+    print(fileName, 'already exists')
+    sys.exit(1)
 
 
 def createHeader(className, nameSpaces, inline):
 
-   fileBase = fullName(className, nameSpaces)
-   fileName =  fileBase + '.h'
-   abortIfExists(fileName)
+    fileBase = fullName(className, nameSpaces)
+    fileName = fileBase + '.h'
+    abortIfExists(fileName)
 
-   indent = 0
-   with open(fileName, 'w') as outfile:
+    indent = 0
+    with open(fileName, 'w') as outfile:
 
-      def fprint(line):
-         outfile.write(' ' * 3 * indent + line + '\n')
+        def fprint(line):
+            outfile.write(' ' * 3 * indent + line + '\n')
 
-      fprint(f'#ifndef {fileBase}H')
-      fprint(f'#define {fileBase}H')
-      fprint('')
+        fprint(f'#ifndef {fileBase}H')
+        fprint(f'#define {fileBase}H')
+        fprint('')
 
-      if nameSpaces:
-         for nameSpace in nameSpaces:
-            fprint(f'namespace {nameSpace}')
-            fprint('{')
-            indent += 1
+        if nameSpaces:
+            for nameSpace in nameSpaces:
+                fprint(f'namespace {nameSpace}')
+                fprint('{')
+                indent += 1
 
-      fprint(f'class {className}')
-      fprint('{')
-      fprint('public:')
-      indent += 1
-      if inline:
-         fprint(f'inline {className}();')
-      else:
-         fprint(f'{className}();')
-      indent -= 1
-      fprint('};')
+        fprint(f'class {className}')
+        fprint('{')
+        fprint('public:')
+        indent += 1
+        if inline:
+            fprint(f'inline {className}();')
+        else:
+            fprint(f'{className}();')
+        indent -= 1
+        fprint('};')
 
-      if nameSpaces:
-         for nameSpace in reversed(nameSpaces):
-            indent -= 1
-            fprint(f'\u007d // namespace {nameSpace}')
+        if nameSpaces:
+            for nameSpace in reversed(nameSpaces):
+                indent -= 1
+                fprint(f'\u007d // namespace {nameSpace}')
 
-      if inline:
-         fprint('')
-         fprint(f'#include "{fileBase}.hpp"')
+        if inline:
+            fprint('')
+            fprint(f'#ifndef {fileBase}HPP')
+            fprint(f'#include "{fileBase}.hpp"')
+            fprint(f'#endif // NOT {fileBase}HPP')
 
-      fprint('')
-      fprint(f'#endif // {fileBase}H')
+        fprint('')
+        fprint(f'#endif // NOT {fileBase}H')
+
 
 def createSource(className, nameSpaces):
 
-   fileBase = fullName(className, nameSpaces)   
-   fileName = fileBase + '.cpp'
-   abortIfExists(fileName)
+    fileBase = fullName(className, nameSpaces)
+    fileName = fileBase + '.cpp'
+    abortIfExists(fileName)
 
-   nameSpace = fullNameSpace(nameSpaces)
-  
-   with open(fileName, 'w') as outfile:
+    nameSpace = fullNameSpace(nameSpaces)
 
-      def fprint(line):
-         outfile.write(line + '\n')
+    with open(fileName, 'w') as outfile:
 
-      fprint(f'#include "{fileBase}.h"')
-      fprint('')
-      fprint(f'{nameSpace}{className}::{className}()')
-      fprint('{')
-      fprint('}')
-      fprint('')      
-   
+        def fprint(line):
+            outfile.write(line + '\n')
+
+        fprint(f'#include "{fileBase}.h"')
+        fprint('')
+        fprint(f'{nameSpace}{className}::{className}()')
+        fprint('{')
+        fprint('}')
+        fprint('')
+
+
 def createInline(className, nameSpaces):
-   
-   fileBase = fullName(className, nameSpaces)   
-   fileName = fileBase + '.hpp'
-   abortIfExists(fileName)
 
-   nameSpace = fullNameSpace(nameSpaces)
+    fileBase = fullName(className, nameSpaces)
+    fileName = fileBase + '.hpp'
+    abortIfExists(fileName)
 
-   indent = 0
-   with open(fileName, 'w') as outfile:
+    nameSpace = fullNameSpace(nameSpaces)
 
-      def fprint(line):
-         outfile.write(' ' * 3 * indent + line + '\n')
+    indent = 0
+    with open(fileName, 'w') as outfile:
 
-      fprint(f'#ifndef {fileBase}HPP')
-      fprint(f'#define {fileBase}HPP')
-      fprint('')         
-      fprint(f'#include "{fileBase}.h"')
-      fprint('')
+        def fprint(line):
+            outfile.write(' ' * 3 * indent + line + '\n')
 
-      fprint(f'{nameSpace}{className}::{className}()')
-      fprint('{')
-      fprint('}')
-      
-      fprint('')
-      fprint(f'#endif // {fileBase}HPP')      
+        fprint(f'#ifndef {fileBase}HPP')
+        fprint(f'#define {fileBase}HPP')
+        fprint('')
+        fprint(f'#include "{fileBase}.h"')
+        fprint('')
+
+        fprint(f'{nameSpace}{className}::{className}()')
+        fprint('{')
+        fprint('}')
+
+        fprint('')
+        fprint(f'#endif // NOT {fileBase}HPP')
+
 
 def main():
 
-   parser = argparse.ArgumentParser(description = 'Create new C++ classes.')
-   parser.add_argument('classnames', metavar='CLASSES', type = str, nargs = '+', help = 'list of classes to create')
-   parser.add_argument('-n', '--namespace', action='append', help = 'all classes are in this namespace')
-   parser.add_argument('-i', '--inline', action = 'store_true', help = 'create HPP instead of CPP')
-      
-   args = parser.parse_args() # will quit here if help is called
+    parser = argparse.ArgumentParser(description='Create new C++ classes.')
+    parser.add_argument('classnames', metavar='CLASSES', type=str, nargs='+', help='list of classes to create')
+    parser.add_argument('-n', '--namespace', action='append', help='all classes are in this namespace')
+    parser.add_argument('-i', '--inline', action='store_true', help='create HPP instead of CPP')
 
-   classNames = args.classnames
-   nameSpaces = args.namespace  
-   inline = args.inline
+    args = parser.parse_args()  # will quit here if help is called
 
-   for className in classNames:
+    classNames = args.classnames
+    nameSpaces = args.namespace
+    inline = args.inline
 
-      createHeader(className, nameSpaces, inline)
-      if inline:
-         createInline(className, nameSpaces)
-      else:
-         createSource(className, nameSpaces)
-  
+    for className in classNames:
+
+        createHeader(className, nameSpaces, inline)
+        if inline:
+            createInline(className, nameSpaces)
+        else:
+            createSource(className, nameSpaces)
+
+
 if __name__ == '__main__':
-   main()
+    main()
